f66b6ab3ae6a9ca369a8728338d5936d317dda3c
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-12-04 21:03:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ app.use(express.static(path.join(__dirname, "public")))
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
+app.use(function(req, res){
+    res.status(404).send("🍌, 404")
+});
 app.get("/", (req, res) => { res.status(200).send("some stuff"); }); 
 app.listen(port, () => { console.log(`Listening to requests on http://localhost:${port}`); });
