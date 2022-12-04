1e20ffe08024d413520c4052a4c9f73ae8dbd461
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-12-04 22:12:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express();
 const port = process.env.PORT || "8000"; 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
